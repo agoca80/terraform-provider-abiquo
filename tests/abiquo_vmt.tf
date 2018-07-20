8e99fc9ddf3646be307539422fb06219b89d7c8d
antxon <antxon.gonzalez@abiquo.com>
2018-07-20 13:00:50 +0200

    Fix variable name

--- tests/abiquo_vmt.tf
+++ tests/abiquo_vmt.tf
@@ -2,10 +2,10 @@ resource "abiquo_vmt" "test" {
   cpu         = 1
   ram         = 64
   repo        = "${data.abiquo_repo.repo.id}"
-  file        = "${var.ova}"
+  file        = "${var.test_ova}"
   name        = "test vmt"
   description = "test vmt"
 }
 
-variable "ova" {  }
+variable "test_ova" {  }
 data     "abiquo_repo" "repo" { datacenter = "datacenter 1" }
