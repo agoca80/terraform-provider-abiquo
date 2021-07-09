003a6264a8f138c5cb6e36c80790427ea2abd9ee
Antxon <antxon.gonzalez@clarivate.com>
2021-07-09 14:06:07 +0200

    Execute coverage tests automatically

--- tests.sh
+++ tests.sh
@@ -3,5 +3,5 @@
 TS=$(date +%s)
 cd ../../hashicorp/terraform
 make testacc TEST=../../abiquo/terraform-provider-abiquo TESTARGS="$* -coverprofile /tmp/coverage-$TS"
-echo go tool cover -html=/tmp/coverage-$TS
+go tool cover -html=/tmp/coverage-$TS
 
